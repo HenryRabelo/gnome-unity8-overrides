68b6e771407ddddd6f8958063731c963a62659bc
HenryRabelo <53522727+HenryRabelo@users.noreply.github.com>
2022-01-26 22:40:12 -0300

    Add files via upload

--- install-overrides.sh
+++ install-overrides.sh
@@ -8,11 +8,11 @@ echo '[Settings]' >> ~/.config/gtk-3.0/settings.ini
 echo 'gtk-modules="appmenu-gtk-module"' >> ~/.config/gtk-3.0/settings.ini
 
 # GSchema Overrides #
-sudo cp ./backgrounds/Saucy-Salamander.jpg /usr/share/backgrounds/gnome/
+sudo cp ./backgrounds/Saucy-Salamander.jpg /usr/share/backgrounds/
 sudo cp ./overrides/*.gschema.override /usr/share/glib-2.0/schemas/
-glib-compile-schemas --strict /usr/share/glib-2.0/schemas/
+sudo glib-compile-schemas --strict /usr/share/glib-2.0/schemas/
 
 # Dash to Dock Override #
-sudo cp /usr/share/themes/Yaru-dark/gnome-shell/gnome-shell.css /usr/share/themes/Yaru-dark/gnome-shell/gnome-shell.css.old
-cat ./overrides/dash-background-mod.css >> /usr/share/themes/Yaru-dark/gnome-shell/gnome-shell.css
+sudo cp /usr/share/themes/Yaru/gnome-shell/gnome-shell.css /usr/share/themes/Yaru/gnome-shell/gnome-shell.css.old
+cat ./overrides/dash-background-mod.css >> /usr/share/themes/Yaru/gnome-shell/gnome-shell.css
 
